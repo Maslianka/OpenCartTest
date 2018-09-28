75177dc5e4308b838bb4adb691fd893ecdac79cf
Nazar <31036073+Maslianka@users.noreply.github.com>
2018-09-28 19:16:22 +0300

    Update UnitTest1.cs

--- OpenCartTest/OpenCartTest/UnitTest1.cs
+++ OpenCartTest/OpenCartTest/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -33,11 +33,24 @@ namespace OpenCartTest
         }
 
         [Test]
-        public void Test_add_products_to_cart()
+        public void Test_add_products_to_cart_first_type()
         {
             // Steps                       
             driver.FindElement(By.XPath("//div[2]/div[1]/div/div[3]/button[1]")).Click();
             driver.FindElement(By.XPath("//ul/li[4]/a/i")).Click();
+            IWebElement actual = driver.FindElement(By.LinkText("MacBook"));
+
+            //assert
+            Assert.True(actual.Text.Contains("MacBook"));
+        }
+
+        [Test]
+        public void Test_add_products_to_cart_second_type()
+        {
+            // Steps                       
+            driver.FindElement(By.XPath("//a[text() = 'MacBook']")).Click();//Find element 'MacBook'
+            driver.FindElement(By.cssSelector(".btn.btn-primary.btn-lg.btn-block")).Click();
+            driver.FindElement(By.cssSelector("//span[text() = 'Shopping Cart']")).Click(); 
             IWebElement actual = driver.FindElement(By.LinkText("MacBook"));
 
             //assert
